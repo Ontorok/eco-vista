01f5afa042e8e081231724a753642fa12a40d870
Nasir Ahmed <nasir.amath07@gmail.com>
2025-07-16 16:28:28 +0600

    feat: add dynamic data for temperature widget

--- _components/CurrentTemperature.jsx
+++ _components/CurrentTemperature.jsx
@@ -1,22 +1,16 @@
+import { getTemperatureData } from '@/_lib/weather-info';
 import Image from 'next/image';
+import WidgetCard from './common/WidgetCard';
 
-export default function CurrentTemperature() {
+export default async function CurrentTemperature({ lat, lon }) {
+  const { temp, feels_like } = await getTemperatureData(lat, lon);
   return (
-    <div className="col-span-12 lg:col-span-4 2xl:col-span-3">
-      <div className="card">
-        <h6 className="feature-name">Current Temperature</h6>
-        <div className="feature-main">
-          <Image
-            width={71}
-            height={71}
-            className="max-w-20"
-            src="/icon_tempareture.png"
-            alt="rain icon"
-          />
-          <h3 className="feature-title">246.43°C</h3>
-          <span className="feature-name">Feels Like 244°C</span>
-        </div>
+    <WidgetCard title={'Current Temperature'} size="sm">
+      <div className="feature-main">
+        <Image width={71} height={71} className="max-w-20" src="/icon_tempareture.png" alt="rain icon" />
+        <h3 className="feature-title">{temp}°C</h3>
+        <span className="feature-name">Feels Like {feels_like}°C</span>
       </div>
-    </div>
+    </WidgetCard>
   );
 }
--- app/[location]/@temperature/page.js
+++ app/[location]/@temperature/page.js
@@ -1,7 +1,5 @@
 import CurrentTemperature from '@/_components/CurrentTemperature';
-import { sleep } from '@/_utils/common-utils';
 
-export default async function CurrentTemperaturePage() {
-  await sleep(3000);
-  return <CurrentTemperature />;
+export default function CurrentTemperaturePage({ params: { location }, searchParams: { latitude, longitude } }) {
+  return <CurrentTemperature lat={latitude} lon={longitude} />;
 }
